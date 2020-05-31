1b9bdde2bbbe0438a7f7c673d0f89e0ec603e684
Phalgun Mittal <phalgun99@gmail.com>
2020-05-31 13:25:02 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+import './App.css';
+
+class App extends React.Component {
+  
+  rendor(){
+    return(
+      <h1>Welcome</h1>
+    );
+  }
+}
+export default App;
